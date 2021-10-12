d82e8818addb22f80688300c6cd9943dff718397
Jordan Ophoff <noreply>
2021-10-11 21:55:40 -0400

    adding additional js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("your robot's name is "+ playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+// fight();
